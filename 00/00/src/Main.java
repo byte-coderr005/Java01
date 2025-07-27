4568a1a5d5e4c946be4867b84a94954ba63028d5
Burak öztürk <burak.ozturkk05@gmail.com>
2025-07-27 16:56:34 +0300

    00

---
+++ 00/00/src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    //public: Erişim belirleyici > Herkes ulaşabilir demek.
+        //static > Nesne oluşturmadan çağrılabilir demek.
+        //void Geriye değer döndürmeyen metod demek.
+        //main metodun adı.
+        //String[] args > komut satırından gelen argumanları alır.
+        //args > dışarıdan gelen değerleri al.
+    }
+}
